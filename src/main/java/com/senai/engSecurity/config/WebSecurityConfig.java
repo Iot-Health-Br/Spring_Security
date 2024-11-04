aad33f2b70c6bc117623e46e3873585d71395f5e
Iot-Health-Br <igl_macedo@hotmail.com>
2024-11-03 21:04:59 -0300

    V.1.1 - Alteração da regra de segurança sobre o acesso do banco de dados H2.

--- src/main/java/com/senai/engSecurity/config/WebSecurityConfig.java
+++ src/main/java/com/senai/engSecurity/config/WebSecurityConfig.java
@@ -4,19 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -37,11 +31,15 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        http.cors(Customizer.withDefaults())
-                .csrf(AbstractHttpConfigurer::disable)
+        http
+                .cors(Customizer.withDefaults())
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers("/h2-console/**")) // Modificada a configuração do CSRF para ignorar requisições do console H2:
+                .headers(headers -> headers
+                        .frameOptions(frame -> frame.sameOrigin())) // Adiciona a configuração para permitir frames da mesma origem:
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(URL + "/").permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "user").permitAll()
                         .requestMatchers(HttpMethod.POST, "user/login").permitAll()
                         .requestMatchers(URL + "api/user").hasAnyRole("USER", "ADM")
@@ -51,5 +49,4 @@ public class WebSecurityConfig {
 
         return http.build();
     }
-
 }
